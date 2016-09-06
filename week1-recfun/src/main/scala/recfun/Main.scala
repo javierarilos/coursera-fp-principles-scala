e344d89ed3f7a99fe20db56f032dbc7e0c3ba9aa
javierarilos <javier.arilos@gmail.com>
2016-09-06 17:09:13 +0200

    countChange exercise solved.

--- week1-recfun/src/main/scala/recfun/Main.scala
+++ week1-recfun/src/main/scala/recfun/Main.scala
@@ -11,6 +11,7 @@ object Main {
   }
 
   /**
+    *
     * Exercise 1
     */
   def pascal(c: Int, r: Int): Int = {
@@ -37,5 +38,12 @@ object Main {
   /**
     * Exercise 3
     */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0)
+      1
+    else if(money > 0 && !coins.isEmpty)
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else
+      0
+  }
 }
