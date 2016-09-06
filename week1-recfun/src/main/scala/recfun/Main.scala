3a96c79d8a9d54e39d11c05c09573e2b82ca5572
javierarilos <javier.arilos@gmail.com>
2016-09-06 16:37:46 +0200

    pascal and balance exercises solved

--- week1-recfun/src/main/scala/recfun/Main.scala
+++ week1-recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,31 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c <= 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def openedBalance(chars: List[Char], opened: Int): Boolean = {
+      if (chars.isEmpty) opened == 0
+      else
+      if (chars.head == '(') openedBalance(chars.tail, opened + 1)
+      else
+      if (chars.head == ')') opened > 0 && openedBalance(chars.tail, opened - 1)
+      else openedBalance(chars.tail, opened)
+    }
+
+    openedBalance(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
